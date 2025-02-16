2bf593f0aa47ac3f442ede236eb74d0159045a21
Wabuluka <dwabuluka@gmail.com>
2025-02-16 15:10:36 -0600

    update

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
   res.send({ hi: "Hello World" });
 });
 
+app.get("/checkstatus", (req, res) => {
+  res.send({ hey: "Everything is working well!" });
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);

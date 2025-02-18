e0d209f55bc473f4a1d5546d99b91d4b21c51688
Wabuluka <dwabuluka@gmail.com>
2025-02-17 18:03:52 -0600

    adding passport google strategy

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("express-session");
+const passport = require("passport");
+const keys = require("./config/keys");
+require("./models/User");
+require("./services/passport");
+
+mongoose.connect(keys.localMongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "Hello World" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    resave: false,
+    saveUninitialized: false,
+    secret: keys.cookieKey,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/checkstatus", (req, res) => {
-  res.send({ hey: "Everything is working well!" });
-});
+require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log("connection success full", PORT));
